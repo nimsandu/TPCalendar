d4534cd58c05a5b6c1d0d3401b7bd316478131f1
Nimsandu Kottage <57834844+nimsandu@users.noreply.github.com>
2025-02-23 13:12:54 +0530

    pwa build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'The Poets Calendar',
+        short_name: 'TPCalendar',
+        description: 'tp calendar beta preview v0.1',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/pwa-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+      },
+    }),
+  ],
+});
